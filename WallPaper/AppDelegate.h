4077cc92c78344f3488afb3273805ba11c3be112
James VanBeverhoudt <jvanbev1@gmail.com>
2014-06-06 00:32:41 -0400

    initial Commit

---
+++ WallPaper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WallPaper
+//
+//  Created by James VanBeverhoudt on 6/6/14.
+//  Copyright (c) 2014 noOrg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WallPaper/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WallPaper
+//
+//  Created by James VanBeverhoudt on 6/6/14.
+//  Copyright (c) 2014 noOrg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
